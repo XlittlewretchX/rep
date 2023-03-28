a314f3560ff66b68d133b3f8506eb5b0cddb0a07
XlittlewretchX <saharovnikita498@gmail.com>
2023-03-28 18:50:52 +0300

    beta

--- pars.py
+++ pars.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,8 @@ response = requests.get(url)
 with open("./python_is_cool.html", 'wb') as f:
     f.write(response.content)
 
-query = 'cats'
+query = sys.argv[1] if len(sys.argv) > 1 else input('Ввeдите тип вашего аватара: ')
+
 urls = f'https://www.kiddle.co/s.php?q={query}'
 page = requests.get(urls).text
 soup = BeautifulSoup(page, 'html.parser')
@@ -19,4 +21,7 @@ for raw_img in soup.find_all('img'):
         response = requests.get(link)
         with open("./today_avatar.jpg", 'wb') as f:
             f.write(response.content)
+        print('Аватар найден - today_avatar.jpg')
         break
+else:
+    print('Аватар не найден - today_avatar.jpg')
